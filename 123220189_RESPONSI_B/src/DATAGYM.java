4e655c895aca45565e06f44f137634a447eba1bb
NadiaNurHalisa <161995535+NadiaNurHalisa@users.noreply.github.com>
2024-05-21 10:03:57 +0700

    Add files via upload

---
+++ 123220189_RESPONSI_B/src/DATAGYM.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lab Informatika
+ */
+public class DATAGYM {
+    package model;
+    public class DATAGYM {
+        private integer id;
+        private string Nama Penyewa;
+        private string Nama Pemilik;
+        private string hari;
+        private string lokasi;
+        
+        
+    }
+}
---
+++ 123220189_RESPONSI_B/src/datasewa.java
@@ -0,0 +1,37 @@
+
+
+import java.io.*;
+import java.awt.*;
+import java.awt.datasewa*;
+import javax.swing.*;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lab Informatika
+ */
+public class datasewa {
+    class sewatends JFrame;
+            private JTextField txTempatGym;
+            private string[] vehicleTypes = {"tempay gym"};
+            private string[] vehicleprices = {"RP 25.000 , RP 50.000 "};
+            private string selectedVehicle Type;
+            private JLabel iblDetails,IblTotalPrice;
+            
+            publicdatasewa{
+    frame = new JFrame ("sewagym");
+    frame.setDefaultCloseOperation();
+    frame.setSize(400,300);
+    
+    create VehicleSelectionPanel();
+    frame.setVisible(true);
+    
+    
+}
+                 
+}
